405937f07b048e1878988ceb2cdeb9b115ff1531
lincolnge <326684793@qq.com>
2013-11-30 14:41:51 +0800

    add thread to messgaeClient

--- messageClient.java
+++ messageClient.java
@@ -3,30 +3,36 @@ import java.util.*;
 import java.rmi.*;
 import java.net.*;
 public class messageClient {
-  public static void main(String args[]) throws IOException {
-    for (; ; ) {
-      BufferedReader br = new BufferedReader (
-              new InputStreamReader(System.in));
-      String str = br.readLine();
-      String lt = null;
-      if (str.equals("exit"))
-        break;
-      
-      try {
-        Object o = Naming.lookup("connect");
-        receiveMessageInterface ts = (receiveMessageInterface) o;
-        // ts.receiveMessage(args[0]);
-        
-        lt = ts.receiveMessage("Client: " + str);
-      } catch (MalformedURLException ex) {
-        // System.err.println(args[0] + " is not a valid RMI URL");
-      } catch (RemoteException ex) {
-        System.err.println("Remote object threw exception " + ex);
-      } catch (NotBoundException ex) {
-        System.err.println("Could not find the requested remote object on the server");
-      }
-      System.out.println("The other client: " + lt);
+  public static void main(String args[]) {
+    (new Thread(new Runner())).start();
+  }
+}
+
+class Runner implements Runnable {
+  public void run () {
+    BufferedReader br = new BufferedReader (
+            new InputStreamReader(System.in));
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
-    
+    String lt = null;
+
+    try {
+      Object o = Naming.lookup("connect");
+      receiveMessageInterface ts = (receiveMessageInterface) o;
+      // ts.receiveMessage(args[0]);
+      
+      lt = ts.receiveMessage("Client: " + str);
+    } catch (MalformedURLException ex) {
+      // System.err.println(args[0] + " is not a valid RMI URL");
+    } catch (RemoteException ex) {
+      System.err.println("Remote object threw exception " + ex);
+    } catch (NotBoundException ex) {
+      System.err.println("Could not find the requested remote object on the server");
+    } 
   }
 }
